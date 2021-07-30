b93124ab1482a6292c6ccf5b97a452e2c95a1db0
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-07-30 16:05:06 +0200

    Updated tests

--- tests/test_acia.py
+++ tests/test_acia.py
@@ -6,6 +6,7 @@ import pytest
 
 
 from acia import acia
+from acia.segm.omero.shapeUtils import make_coordinates
 
 
 @pytest.fixture
@@ -22,3 +23,6 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+def test_coordinates():
+    assert make_coordinates("20,13 34,-4") == [(20, 13), (34, -4)]
