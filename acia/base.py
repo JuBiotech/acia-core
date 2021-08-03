47fc3080e7508d3022e28e60a3cb8f36286b6eba
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-08-03 09:36:22 +0200

    Updated Contour implementation
    - added plus operator for merges
    - added time iterator

--- acia/base.py
+++ acia/base.py
@@ -22,6 +22,29 @@ class Overlay:
     def __iter__(self):
         return iter(self.contours)
 
+    def __add__(self, other):
+        jointContours = self.contours + other.contours
+        return Overlay(jointContours)
+
+    def timeIterator(self, startFrame=0, endFrame=None):
+        '''
+            Creates an iterator that returns an Overlay for every frame between starFrame and endFrame
+
+            startFrame: first frame number
+            endFrame: last frame number
+        '''
+        assert startFrame >= 0
+        if endFrame is None:
+            # automatically determine the max endTime
+            endFrame = max([c.frame for c in self.contours])
+        else:
+            assert endFrame >= 0
+
+        # iterate frames
+        for frame in range(startFrame, endFrame+1):
+            # filter sub overlay with all contours in the frame
+            yield Overlay(list(filter(lambda contour: contour.frame == frame, self.overlay)))
+
 
 class Processor(object):
     pass
