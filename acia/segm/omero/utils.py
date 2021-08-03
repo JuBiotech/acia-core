dc071062e4b95ec84276916f5316ee08b228af3e
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-08-03 09:36:37 +0200

    Added omero utility functions

--- acia/segm/omero/utils.py
+++ acia/segm/omero/utils.py
@@ -1,2 +1,12 @@
-def list_images(conn, datasetId: int):
+def list_images_in_dataset(conn, datasetId: int):
     return [image.getId() for image in conn.getObjects('Image', opts={'dataset': datasetId})]
+
+
+def list_images_in_project(conn, projectId: int):
+    return [image.getId() for dataset in conn.getObjects('Dataset', opts={'project': projectId}) for image in dataset.listChildren()]
+
+def get_image_name(conn, imageId: int):
+    return conn.getObject('Image', imageId).getName()
+
+def get_project_name(conn, projectId: int):
+    return conn.getObject('Project', projectId).getName()
