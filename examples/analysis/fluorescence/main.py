3266741a2dc8995c8e048777a157ffecb41de2ae
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-09-07 14:42:19 +0200

    Make output directory

--- examples/analysis/fluorescence/main.py
+++ examples/analysis/fluorescence/main.py
@@ -1,8 +1,13 @@
+import os
+
 from fluorescence_analysis import main as fa_main
 from datapoints_analysis import main as da_main
 from videoRender import main as vr_main
+from config import basepath
 
 if __name__ == '__main__':
+    os.makedirs(basepath)
+
     # execute the three analysis steps to produce fluorescence results
     fa_main()
     da_main()
