2066110d207bb0d5651deb2d040bf46c8b1711bf
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-09-07 10:26:34 +0200

    Updated contour and IRS implementation
    - improved masking feature
    - support drawing contours
    - added apply, apply_parallel and apply_parallel_star functions

--- acia/base.py
+++ acia/base.py
@@ -1,12 +1,20 @@
-from typing import Iterator, List, Tuple
+from __future__ import annotations
+
+from typing import Callable, Iterator, List, Tuple
 import numpy as np
 from PIL import Image, ImageDraw
+import tqdm
+from functools import partial
+
+def unpack(data, function):
+    return function(*data)
 
 class Contour:
-    def __init__(self, coordinates, score, frame):
+    def __init__(self, coordinates, score, frame, id):
         self.coordinates = coordinates
         self.score = score
         self.frame = frame
+        self.id = id
 
     '''
         Render contour mask onto existing image
@@ -15,8 +23,10 @@ class Contour:
         fillValue: mask values inside the contour
         outlineValues: mask values on the outline (border)
     '''
-    def _toMask(self, img, maskValue=1, outlineValue=1):
-        ImageDraw.Draw(img).polygon(self.coordinates, outline=outlineValue, fill=maskValue)
+    def _toMask(self, img, maskValue=1, outlineValue=1, draw=None):
+        if draw is None:
+            draw = ImageDraw.Draw(img)
+        draw.polygon(self.coordinates, outline=outlineValue, fill=maskValue)
         mask = np.array(img, np.bool)
 
         return mask
@@ -32,7 +42,12 @@ class Contour:
     def toMask(self, height, width, fillValue=1, outlineValue=1):
         img = Image.new('L', (width, height), 0)
 
-        return self.__toMask(img, maskValue=fillValue, outlineValue=outlineValue)
+        return self._toMask(img, maskValue=fillValue, outlineValue=outlineValue)
+
+    def draw(self, image, draw=None, outlineColor=(255, 255, 0), fillColor=None):
+        if draw is None:
+            draw = ImageDraw.Draw(image)
+        draw.polygon(self.coordinates, outline=outlineColor, fill=fillColor)
 
 
 class Overlay:
@@ -103,6 +118,19 @@ class Overlay:
 
         return masks
 
+    def draw(self, image, outlineColor: str | Callable[[Contour], Tuple[int]] = None, fillColor: str | Callable[[Contour], Tuple[int]] = None):
+        for timeOverlay in self.timeIterator():
+            for cont in timeOverlay:
+                oc_local = outlineColor
+                fc_local = fillColor
+
+                if oc_local and isinstance(oc_local, Callable):
+                    oc_local = oc_local(cont)
+                if fc_local and isinstance(fc_local, Callable):
+                    fc_local = fc_local(cont)
+
+                cont.draw(image, outlineColor=oc_local, fillColor=fc_local)
+
 
 class Processor(object):
     pass
@@ -124,3 +152,35 @@ class ImageRoISource(object):
 
     def __iter__(self) -> Iterator[Tuple[np.array, Overlay]]:
         return zip(iter(self.imageSource), iter(self.roiSource))
+
+    def __len__(self):
+        return min(len(self.imageSource), len(self.roiSource))
+
+    def apply_parallel(self, function, num_workers=None):
+        import multiprocessing
+        from tqdm.contrib.concurrent import process_map
+        if num_workers is None:
+            num_workers = int(np.floor(multiprocessing.cpu_count()*2/3))
+
+        def limit():
+            for i, el in enumerate(self):
+                yield el
+
+        return process_map(function, self, max_workers=num_workers, chunksize=4)
+
+    def apply_parallel_star(self, function, num_workers=None):
+        import multiprocessing
+        from tqdm.contrib.concurrent import process_map
+        if num_workers is None:
+            num_workers = int(np.floor(multiprocessing.cpu_count()*2/3))
+
+        return process_map(partial(unpack, function=function), self, max_workers=num_workers, chunksize=4)
+
+    def apply(self, function):
+        def limit():
+            for i, el in enumerate(self):
+                yield el
+                #if i == 11:
+                #    break
+
+        return list(tqdm.tqdm(map(function, limit())))
