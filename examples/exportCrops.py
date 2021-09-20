d2383f3b14168478a184c75003eeb369ce98f2f3
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-09-20 13:11:34 +0200

    Added cropping example

---
+++ examples/exportCrops.py
@@ -0,0 +1,24 @@
+from acia.segm.local import ImageJRoISource, LocalSequenceSource
+import os.path as osp
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+
+    local_tiff_file = osp.join('simulated_data/00.tif')
+
+    rs = ImageJRoISource(local_tiff_file)
+    ss = LocalSequenceSource(local_tiff_file)
+
+    vertical_slice = slice(350,550)
+    horizontal_slice = slice(225,425)
+
+    for frame, (overlay, image) in enumerate(zip(rs, ss)):
+        image = cv2.drawContours(image, [np.array(cont.coordinates).astype(np.int32) for cont in overlay.contours], -1, (255, 255, 0)) # RGB format
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        image = image[vertical_slice, horizontal_slice]
+
+        cv2.imwrite('images/%02d.png' % frame, image)
+
