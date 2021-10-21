b317aa196214b193e9ac2f3b98cbfe9ad4d1b5b5
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-10-21 20:17:20 +0200

    Added annotation deletion example

---
+++ examples/deleteAnnotations.py
@@ -0,0 +1,12 @@
+"""
+    Delete segmentations of an omero image
+"""
+
+from acia.segm.omero.storer import OmeroRoIStorer
+
+
+if __name__ == '__main__':
+    # imageId: We want to delete the RoIs from this image
+    imageId = 262
+
+    OmeroRoIStorer.clear(imageId, 'root', 'omero', 'ibt056')
