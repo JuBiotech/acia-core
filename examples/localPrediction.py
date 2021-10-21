dadfffc20b5f99d88d32c566dd1ec6a2e41fc307
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-10-21 18:33:41 +0200

    Updated local predictions

--- examples/localPrediction.py
+++ examples/localPrediction.py
@@ -1,16 +1,21 @@
-from acia.deploy import OfflineModel, LocalSequenceSource, RoiStorer
+from acia.segm.processor.offline import OfflineModel
+from acia.segm.local import LocalSequenceSource, RoiStorer
+from exportVideo import renderVideo
 
 if __name__ == '__main__':
     '''
         Simple example to use the apis
     '''
     # create local machine learning model
-    model = OfflineModel('train_work_dirs/train_data/me_htc_2_False_True_15_True_12/me_htc_2_False_True_15_True_12.py', 'train_work_dirs/train_data/me_htc_2_False_True_15_True_7/latest.pth', half=True)
+    model = OfflineModel('model_zoo/htc/htc_tuned.py', 'model_zoo/htc/latest.pth', half=True)
     # create local image data source
-    source = LocalSequenceSource('input/PHH2.nd2-PHH2.nd2(series6)_rois-1_70_final.tif')
+    source = LocalSequenceSource('input/nd074_crop01.nd2 - nd074_crop01.nd2 (series 7)-6.tif')
 
     # perform overlay prediction
-    result = model.predict(source)
+    result = model.predict(source.slice(0,40), tiling=True)
+
+    print('Render Video...')
+    renderVideo(source.slice(0,40), result.timeIterator())
 
     # store in rois
     RoiStorer.store(result, 'rois.zip')
