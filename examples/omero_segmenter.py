7e87ef5476a3c35f7d18b86e0bb1ee51225588d2
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-10-26 09:47:15 +0200

    Added example
    - automated background segmentation on omero

--- examples/omero_segmenter.py
+++ examples/omero_segmenter.py
@@ -4,23 +4,13 @@ from numpy import integer
 from omero.gateway import BlitzGateway
 from acia.base import Processor
 from acia.segm.omero.storer import OmeroRoIStorer, OmeroSequenceSource
-from acia.segm.omero.utils import list_images_in_dataset, list_images_in_project
+from acia.segm.omero.utils import image_iterator, list_images_in_dataset, list_images_in_project
 from acia.segm.processor.offline import OfflineModel
 from acia.segm.filter import NMSFilter
 from examples.training_dataset.exportDataset import has_all_tags
 import time
 import omero
 import itertools
-
-def image_iterator(conn, object):
-    if object.OMERO_CLASS == 'Image':
-        yield object
-    if object.OMERO_CLASS == 'Dataset':
-        for image in list_images_in_dataset(conn, object.getId()):
-            yield image
-    if object.OMERO_CLASS == 'Project':
-        for image in list_images_in_project(conn, object.getId()):
-            yield image
 
 def get_tags(conn, object):
     """Obtain all tags associated with an omero object
@@ -62,7 +52,7 @@ def remove_tag(conn, object, tag_name: str):
 
 def create_model():
     # create local machine learning model
-    return OfflineModel('model_zoo/htc/htc_tuned.py', 'model_zoo/htc/latest.pth', half=True)
+    return OfflineModel('model_zoo/htc/htc_tuned.py', 'model_zoo/htc/latest.pth', half=True, tiling={'x_shift': 768-128, 'y_shift': 768-128, 'tile_height': 768, 'tile_width': 768})
 
 def predict(imageId: integer, model: Processor, conn):
     """[summary]
@@ -77,7 +67,7 @@ def predict(imageId: integer, model: Processor, conn):
 
     # perform overlay prediction
     print("Perform Prediction...")
-    result = model.predict(source, tiling=True)
+    result = model.predict(source)
 
     # filter cell detections
     print("Filter detections")
@@ -86,6 +76,58 @@ def predict(imageId: integer, model: Processor, conn):
     # store detections in omero
     print("Save results...")
     OmeroRoIStorer.storeWithConn(result, imageId, conn=conn)
+
+def segmentation(username, password, serverUrl):
+    tag_filter = ['request-segm'] # e.g. add E. coli here
+
+    request_tags = tag_filter
+
+    model = None
+
+    def get_model():
+        return create_model()
+
+
+    logging.info("Connect to omero...")
+    with BlitzGateway(username, password, host=serverUrl, port=4064, secure=True) as conn:
+        omero_objects = itertools.chain(conn.getObjects("Image"), conn.getObjects("Dataset"), conn.getObjects("Project"))
+
+        # iterate over any kind of omero object (Image, Dataset, Project)
+        for obj in omero_objects:
+            if has_all_tags(obj, tag_filter):
+                print(f'Found segmentation deletion request for {obj.getName()}')
+
+                # iterate over the image(s) in there
+                for image in image_iterator(conn, obj):
+                    
+                    # do the segmentation here
+                    predict(image.getId(), get_model(), conn)
+                    print("Segmentation successfull!")
+
+                # if everything worked well: remove the tag that triggers the execution
+                for tag in request_tags:
+                    remove_tag(conn, obj, tag)
+
+
+def del_segmentations(username, password, serverUrl):
+    tag_filter = ['request-del-segm'] # e.g. add E. coli here
+
+    request_tags = tag_filter
+
+    with BlitzGateway(username, password, host=serverUrl, port=4064, secure=True) as conn:
+        omero_objects = itertools.chain(conn.getObjects("Image"), conn.getObjects("Dataset"), conn.getObjects("Project"))
+
+        # iterate over any kind of omero object (Image, Dataset, Project)
+        for obj in omero_objects:
+            if has_all_tags(obj, tag_filter):
+                print(f'Found segmentation for {obj.getName()}')
+                # iterate over the image(s) in there
+                for image in image_iterator(conn, obj):
+                    OmeroRoIStorer.clear(image.getId(), conn=conn)
+
+                # if everything worked well: remove the tag that triggers the execution
+                for tag in request_tags:
+                    remove_tag(conn, obj, tag)
 
 
 if __name__ == '__main__':
@@ -99,41 +141,10 @@ if __name__ == '__main__':
         'password': password
     }
 
-    tag_filter = ['request-segm'] # e.g. add E. coli here
-
-    request_tags = tag_filter
 
     while True:
-        model = None
 
-        def get_model():
-            global model
-            if model:
-                return model
-            else:
-                model = create_model()
-                return model
-
-
-        logging.info("Connect to omero...")
-        with BlitzGateway(username, password, host=serverUrl, port=4064, secure=True) as conn:
-            omero_objects = itertools.chain(conn.getObjects("Image"), conn.getObjects("Dataset"), conn.getObjects("Project"))
-
-            # iterate over any kind of omero object (Image, Dataset, Project)
-            for obj in omero_objects:
-                if has_all_tags(obj, tag_filter):
-                    print(f'Found segmentation for {obj.getName()}')
-
-                    # iterate over the image(s) in there
-                    for image in image_iterator(conn, obj):
-                        
-                        # do the segmentation here
-                        predict(image.getId(), get_model(), conn)
-                        print("Segmentation successfull!")
-
-                    # if everything worked well: remove the tag that triggers the execution
-                    for tag in request_tags:
-                        remove_tag(conn, obj, tag)
-
+        segmentation(**omero_cred)
+        del_segmentations(**omero_cred)
         print('Sleep')
         time.sleep(30)
