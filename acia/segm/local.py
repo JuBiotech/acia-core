0c3ccc37140c888b68efe7b7dabdccf0073a9c65
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-11-02 08:55:38 +0100

    Update local sources
    - added single image source
    - bundle image preparation logic

--- acia/segm/local.py
+++ acia/segm/local.py
@@ -1,9 +1,52 @@
 import numpy as np
 import tifffile
+import cv2
+import os.path as osp
+import logging
 
 from acia.base import ImageSequenceSource, Overlay, Contour, RoISource
 import roifile
 
+def prepare_image(image, normalize_image=True):
+    """Normalize and convert image to RGB.
+
+    Args:
+        image ([type]): [description]
+        normalize_image (bool, optional): Whether to normalize the image into uint8 domain (0-255). Defaults to True.
+    Returns:
+        [np.array]: RGB image (Width, height, 3 color channels)
+    """
+    # normalize image space
+    if normalize_image:
+        min_val = np.min(image)
+        max_val = np.max(image)
+        image = np.floor((image - min_val) / (max_val - min_val) * 255).astype(np.uint8)
+
+    if len(image.shape) > 2:
+        # select only the first channel
+        image = image[0]
+
+    if len(image.shape) == 2:
+        # make it artificially rgb
+        image = np.repeat(image[:, :, None], 3, axis=-1)
+
+    return image
+
+class LocalImageSource(ImageSequenceSource):
+    def __init__(self, file_path: str, normalize_image=True):
+        self.file_path = file_path
+        self.normalize_image = normalize_image
+
+        if not osp.isfile(self.file_path):
+            logging.warning(f'File "{self.file_path}" does not exist!')
+
+    def __iter__(self):
+        image = cv2.imread(self.file_path)
+
+        yield prepare_image(image, self.normalize_image)
+
+    def __len__(self):
+        return 1
 
 class LocalSequenceSource(ImageSequenceSource):
     def __init__(self, tif_file, normalize_image=True):
@@ -14,19 +57,7 @@ class LocalSequenceSource(ImageSequenceSource):
         images = tifffile.imread(self.filename)
 
         for image in images:
-            # normalize image space
-            if self.normalize_image:
-                min_val = np.min(image)
-                max_val = np.max(image)
-                image = np.floor((image - min_val) / (max_val - min_val) * 255).astype(np.uint8)
-
-            if len(image.shape) > 2:
-                # select only the first channel
-                image = image[0]
-
-            if len(image.shape) == 2:
-                # make it artificially rgb
-                image = np.repeat(image[:, :, None], 3, axis=-1)
+            image = prepare_image(image, self.normalize_image)
 
             yield image
 
