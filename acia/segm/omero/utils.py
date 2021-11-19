5fd7385e50c7d6881e802baf70de0555ad2f84ea
Johannes Seiffarth <johannes.seiffarth@rwth-aachen.de>
2021-11-19 18:35:46 +0100

    Add omero creation methods in utils
    - create project & dataset

--- acia/segm/omero/utils.py
+++ acia/segm/omero/utils.py
@@ -1,5 +1,6 @@
 from typing import List
 from omero.gateway import ImageWrapper, DatasetWrapper, ProjectWrapper, BlitzGateway
+import omero
 
 def getImage(conn: BlitzGateway, imageId: int) -> ImageWrapper:
     """Get omero image by id
@@ -97,3 +98,29 @@ def image_iterator(conn: BlitzGateway, object) -> ImageWrapper:
     if object.OMERO_CLASS == 'Project':
         for image in list_images_in_project(conn, object.getId()):
             yield image
+
+
+def create_project(conn: BlitzGateway, project_name: str)-> ProjectWrapper:
+    new_project = ProjectWrapper(conn, omero.model.ProjectI())
+    new_project.setName(project_name)
+    new_project.save()
+    return new_project
+
+def create_dataset(conn: BlitzGateway, projectId: int, dataset_name: str)-> DatasetWrapper:
+    # Use omero.gateway.DatasetWrapper:
+    new_dataset = DatasetWrapper(conn, omero.model.DatasetI())
+    new_dataset.setName(dataset_name)
+    new_dataset.save()
+    # Can get the underlying omero.model.DatasetI with:
+    dataset_obj = new_dataset._obj
+
+    # Create link to project
+    link = omero.model.ProjectDatasetLinkI()
+    # We can use a 'loaded' object, but we might get an Exception
+    # link.setChild(dataset_obj)
+    # Better to use an 'unloaded' object (loaded = False)
+    link.setChild(omero.model.DatasetI(dataset_obj.id.val, False))
+    link.setParent(omero.model.ProjectI(projectId, False))
+    conn.getUpdateService().saveObject(link)
+
+    return new_dataset
