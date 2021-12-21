8e701d1f4fc222684d1542fc36ebba2c308ee595
j.seiffarth <j.seiffarth@fz-juelich.de>
2021-12-21 15:10:20 +0100

    Added offline processor for Cell/OmniPose

--- acia/segm/processor/offline.py
+++ acia/segm/processor/offline.py
@@ -1,6 +1,10 @@
+import logging
 from acia.base import Contour, ImageSequenceSource, Overlay, Processor
 from mmdet.apis import init_detector
 from mmcv.runner import wrap_fp16_model
+from cellpose import models
+import torch
+import cv2
 
 
 from .predict import contour_from_mask
@@ -76,5 +80,77 @@ class OfflineModel(Processor):
 
             contours = [Contour(cont, pred['score'], frame_id, id=-1) for pred,cont in all_contours]
             overlay.add_contours(contours)
+
+        return overlay
+
+class PoseModel(Processor):
+    '''
+        Model that runs on the local computer
+    '''
+    def __init__(self, model_name = 'bact_omni', omni=True, use_gpu = torch.cuda.is_available(), diameter=None, flow_threshold=None):
+        '''
+            config_file: model configuration file
+            parameter_file: model checkpoint file
+            half: enables half-precision (16-bit) execution. A bit faster.
+            device: chooses the device to execute (e.g. 'cpu' or 'cuda' or 'cuda:0')
+        '''
+        self.omni = omni
+        self.use_gpu = use_gpu
+        self.model_name = model_name
+
+        self.diameter = diameter
+        self.flow_threshold = flow_threshold
+        
+
+
+    def load_model(self):
+        '''
+            Load model from definitions
+        '''
+        logging.info(f'Loading model {self.model_name}')
+        self.model = models.Cellpose(gpu=self.use_gpu, model_type=self.model_name, omni=self.omni)
+
+    def predict(self, source: ImageSequenceSource) -> Overlay:
+        '''
+            Predicts the overlay for an image sequence
+
+            source: image sequence source
+            tiling: whether to enable tiling
+        '''
+        self.load_model()
+        channels = [[0,0]]
+
+
+
+        # TODO: super strange without [] it takes some other list as initialization. This leads to detected cells from other images...
+        overlay = Overlay([])
+
+        for frame_id, image in tqdm.tqdm(enumerate(source)):
+            try:
+                masks, flows, styles, diams = self.model.eval([image], channels=channels, rescale=None, diameter=70, flow_threshold=.9, mask_threshold=.0, resample=True, diam_threshold=100)
+            except:
+                print("Error in OmniPose prediction")
+                masks = [[],]
+
+            int_mask = masks[0]
+
+            num_cells = np.max(int_mask)
+            score_threshold = 0.5
+            all_contours = []
+
+            for index in range(1, num_cells+1):
+                bool_mask = int_mask == index
+
+                contours, hierarchy = cv2.findContours(np.where(bool_mask > score_threshold, 1, 0).astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+                for contour in contours:
+                    contour = np.squeeze(contour)
+                    if len(contour) > 3:
+                        all_contours.append(contour)
+
+            for contour in all_contours:
+                overlay.add_contour(Contour(contour, -1, frame_id, -1))
+
+
 
         return overlay
