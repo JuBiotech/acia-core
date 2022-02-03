0d4bf39454e4eee1b7f18f8d7a2894386ab0e96f
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2022-02-03 16:07:59 +0100

    Added fluorescence tests
    - for parallel execution

--- tests/analysis/test_property_extractor.py
+++ tests/analysis/test_property_extractor.py
@@ -12,9 +12,10 @@ from acia.analysis import (
 )
 import pint
 import numpy as np
+from itertools import product, starmap
 
-from acia.base import Contour, Overlay
-from acia.segm.local import LocalImageSource
+from acia.base import Contour, ImageSequenceSource, Overlay
+from acia.segm.local import InMemorySequenceSource, LocalImage, LocalImageSource
 
 
 class TestPropertExtractors(unittest.TestCase):
@@ -75,6 +76,31 @@ class TestPropertExtractors(unittest.TestCase):
         self.assertEqual(df["position_y"][0], 0.5 * 0.07)
         self.assertEqual(df["gfp"][0], 5.5)
         self.assertEqual(df["gfp_mean"][0], np.mean([2, 5, 6, 10]))
+
+    def test_parallel_fluorescence_extraction(self):
+        squared_num = 30
+        contours = [Contour([[0, 0], [1, 0], [1, 1], [0, 1]], -1, frame=0, id=23) for id,frame in product(list(range(squared_num)), list(range(squared_num)))]
+        overlay = Overlay(contours)
+
+        image = np.zeros((200, 200))
+        image[0, 0] = 2
+        image[0, 1] = 5
+        image[1, 0] = 6
+        image[1, 1] = 10
+        image_sources =  InMemorySequenceSource(np.stack([image] * squared_num))
+
+        # test basic extractors
+        df = ExtractorExecutor().execute(
+            overlay=overlay,
+            images=image_sources,
+            extractors=[
+                FluorescenceEx(channels=[0], channel_names=['fl1']),
+                FluorescenceEx(channels=[0], channel_names=['fl1_mean'], summarize_operator=np.mean),
+            ],
+        )
+
+        np.testing.assert_array_equal(df['fl1'], [5.5] * len(df))
+        np.testing.assert_array_equal(df['fl1_mean'], [np.mean([2, 5, 6, 10])] * len(df))
 
 
 if __name__ == "__main__":
