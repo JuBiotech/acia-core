682f25dd74b1b18c3c0959049fa04c54a36874d7
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2022-09-08 09:01:14 +0200

    Store/load simple segmentation format

--- acia/segm/omero/storer.py
+++ acia/segm/omero/storer.py
@@ -1,5 +1,6 @@
 """Classes for OMERO storage interaction"""
 
+import json
 import logging
 from pathlib import Path
 from typing import Tuple
@@ -783,3 +784,77 @@ def print_file_annotations(omero_type: str, omero_id: int, conn: BlitzGateway):
             "Namespace:",
             ann.getNs(),
         )
+
+
+def parse_simple_segmentation(file_content: str) -> Overlay:
+    """Parse simple segmentation format from string (json)
+
+    Args:
+        file_content (str): simple segmentation file content as string
+
+    Returns:
+        Overlay: the overlay representation of the segmentation
+    """
+
+    file_data = json.loads(file_content)
+
+    contours = []
+
+    for frame in file_data:
+        frame_id = frame["frame"]
+        for det in frame["detections"]:
+            contours.append(
+                Contour(det["contour"], -1.0, frame_id, det["id"], det["label"])
+            )
+
+    return Overlay(contours)
+
+
+def gen_simple_segmentation(overlay: Overlay) -> str:
+    """Create a simple segmentation string from an Overlay
+
+    Args:
+        overlay (Overlay): the overlay to store
+
+    Returns:
+        str: string containing the stringified simple segmentation json format
+    """
+
+    frame_packages = []
+
+    # loop over frames
+    for frame_overlay in overlay.timeIterator():
+        if len(frame_overlay) == 0:
+            continue
+
+        det_objects = []
+        frame_id = -1
+
+        # loop over all contours in a frame
+        for cont in frame_overlay:
+
+            # transform coordinates to list (otherwise not json serializable)
+            coordinates = cont.coordinates
+            if isinstance(coordinates, np.ndarray):
+                coordinates = coordinates.tolist()
+
+            # create detection object
+            det_objects.append(
+                dict(
+                    label=cont.label,
+                    contour=coordinates,
+                    id=cont.id,
+                )
+            )
+
+            frame_id = cont.frame
+
+        # create the frame package
+        frame_package = dict(
+            frame=frame_id,
+            detections=det_objects,
+        )
+        frame_packages.append(frame_package)
+
+    # serialize into json format
+    return json.dumps(frame_packages, separators=(",", ":"))
