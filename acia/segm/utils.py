33ae616503ff5216fee8bc69e820887f745ac82b
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2024-11-12 13:52:39 +0100

    Add colony blob utility function
    - turns single-cell overlay into blobs of colonies

--- acia/segm/utils.py
+++ acia/segm/utils.py
@@ -3,9 +3,11 @@
 from typing import Tuple
 
 import numpy as np
-from shapely.geometry import Polygon
+from shapely.geometry import MultiPolygon, Polygon
+from shapely.ops import unary_union
+from tqdm.auto import tqdm
 
-from acia.base import Contour
+from acia.base import Contour, Overlay
 from acia.utils import pairwise_distances
 
 
@@ -55,3 +57,52 @@ def length_and_area(contour: Contour) -> Tuple[float, float]:
         pairwise_distances(np.array(polygon.minimum_rotated_rectangle.exterior.coords))
     )
     return length, polygon.area
+
+
+def single_cell_overlay_to_colony_blobs(overlay: Overlay, expand=10) -> Overlay:
+    """Computing colony blobs from single-cell overlay
+
+    Args:
+        overlay (Overlay): Single-cell overlay containing the individual cell objects
+        expand (int, optional): The number of pixels to expand single-cell objects in order to form blobs. Defaults to 10.
+
+    Returns:
+        Overlay: Overlay of colony blobs
+    """
+
+    merged_contours = []
+
+    # iterate over frames and all the cell instances
+    for frame_ind, frame_overlay in enumerate(tqdm(overlay.timeIterator())):
+
+        # get all polygons
+        cont_polys = [cont.polygon for cont in frame_overlay]
+
+        # increase their size (like a dilation)
+        oversized_polys = [poly.buffer(expand) for poly in cont_polys]
+
+        # merge all polys
+        intersection = unary_union(oversized_polys)
+
+        # erose the merged polygon
+        i = intersection.buffer(-expand)
+
+        # make it a contour
+        polygons = [i]
+        if isinstance(i, MultiPolygon):
+            polygons = list(i.geoms)
+
+        contours = [
+            Contour(
+                np.array(list(zip(p.exterior.xy))).T.squeeze(), -1, frame_ind, frame_ind
+            )
+            for p in polygons
+        ]
+
+        contours = list(filter(lambda c: len(c.coordinates) >= 3, contours))
+
+        # add merged contour to results
+        merged_contours += contours
+
+    # return new overlay with merged contours
+    return Overlay(merged_contours)
