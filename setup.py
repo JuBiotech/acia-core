bcc1a397b7bfade17d02deb69772794baedc22d6
j.seiffarth <j.seiffarth@fz-juelich.de>
2024-12-10 12:48:32 +0000

    Bump version: 0.2.36 → 0.2.37

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/JojoDevel/acia",
-    version="0.2.36",
+    version="0.2.37",
     zip_safe=False,
 )
